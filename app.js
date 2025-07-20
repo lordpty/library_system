5dc09ef2bc2f972185f8ebb409bf3674911539de
Mohammad Fathi <mohfabdou@gmail.com>
2025-07-20 17:43:28 +0200

    added a favicon

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 const port = 3000;
 
 // Serve static files (CSS, JS, images) from public directory
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(favicon(path.join(__dirname, 'public', 'img' ,'favicon.png')));
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
